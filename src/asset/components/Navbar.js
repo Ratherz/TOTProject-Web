fb304e61b2e44b0b5439f8d0cf5dd228c33daa64
job <nuttakarngg@gmail.com>
2019-09-27 13:43:37 +0700

    Navbar 70%

--- src/asset/components/Navbar.js
+++ src/asset/components/Navbar.js
@@ -5,7 +5,7 @@ import $ from 'jquery'
 class Navbar extends React.Component{
   render(){
     return(        <div className="container-fluid">
-    <nav class="navbar navbar-expand-sm navbar-dark bg-light fixed-top ">
+    <nav class="navbar navbar-expand-sm navbar-dark bg-transparent fixed-top" id="banner">
       <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -48,7 +48,7 @@ class Navbar extends React.Component{
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               เอกสาร/รายงาน
             </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <div class="dropdown-menu dropd" aria-labelledby="navbarDropdownMenuLink">
               <a class="dropdown-item" href="#">Action</a>
               <a class="dropdown-item" href="#">Another action</a>
               <a class="dropdown-item" href="#">Something else here</a>
@@ -64,21 +64,25 @@ class Navbar extends React.Component{
               <a class="dropdown-item" href="#">Something else here</a>
             </div>
           </li>
+          
         </ul>
       </div>
     </nav>
-            </div>);
+ </div>);
   }
 }
 
 $(document).on("scroll", function(){
   if
-    ($(document).scrollTop() > 86){
-    $("#banner").addClass("shrink");
+  ($(document).scrollTop() > 200){
+    $("#banner").removeClass("bg-transparent");
+    $("#banner").addClass("bg-light");
   }
   else
   {
-    $("#banner").removeClass("shrink");
+    $("#banner").addClass("fixed-top");
+    $("#banner").addClass("bg-transparent");
+    $("#banner").removeClass("bg-light");
   }
 });
 export default Navbar;
